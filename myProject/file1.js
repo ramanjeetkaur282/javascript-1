00ef8bf42499af6530bd04f60e3baed000d0e5a8
ramanjeetkaur282 <54283512+ramanjeetkaur282@users.noreply.github.com>
2022-05-13 15:00:59 -0400

    Add files via upload
    New Project Added

---
+++ myProject/file1.js
@@ -0,0 +1,15 @@
+"use strict";
+const greet = (name = "Everyone") => {
+  console.log(`Hello ${name}`);
+};
+greet();
+
+var occupation = "Frontend Developer";
+
+function addOne(x) {
+  console.log(x + 1);
+}
+addOne(5);
+var lastname = "Kaur";
+this.lastname = "Raman";
+console.log(lastname);
